ad8c569b01ea3a104df3c3835658f91ae5c9f388
Mehdi <azertyui20018@gmail.com>
2022-04-16 23:43:53 +0000

    play() function added, project finished

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let playerSelection = prompt(),
-    computerSelection = computerPlay(); // calls the function computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-playerSelection = playerSelection.toLowerCase(); computerSelection.toLowerCase();
-console.log('value of computerSelection is ' + computerSelection); // it should be the same as the value of 'match' in computePlay()
-console.log(playRound(playerSelection, computerSelection));
+let playerSelection,
+    computerSelection;
+winCount = 0,
+    loseCount = 0,
+    tieCount = 0;
+console.log(game());
 
 function computerPlay() {
     // pick a random number (from 0 to 9), and based on it, pick one of the strings
-    let random = Math.trunc(Math.random() * 10),
-        match = (random % 3 === 0) ? 'rock' : (random % 2 === 0) ? 'paper' : 'scissors';
+    let random, match;
+    () => {
+        random = Math.trunc(Math.random() * 10);
+        if (random === 0) {
+            for (let i = 0; random === 0; i++) {
+                random = Math.trunc(Math.random() * 10)
+            }
+        }
+    }
+    match = (random % 3 === 0) ? 'rock' : (random % 2 === 0) ? 'paper' : 'scissors';
     // 3, 6, 9 => rock      2, 4, 8 => paper        1, 5, 7 => scissors
-    // to do: when it's 0, make an anonymous/arrow function that picks a new number, and loop again if it's 0
-    console.log('the random number is ' + random); console.log('the \'match\' value is ' + match);
+    // to do: when it's 0, make an anonymous/arrow function that repeat the selection of a random number
     return match;
 }
 
@@ -20,43 +28,34 @@ function playRound(playerSelection, computerSelection) {
     // each string have two coditions/rules, it's powerful than one and weaker than the other. evaluate the conditions and return the result
     // to do: make playerSelection case insensitive with regex
 
-    if (playerSelection == computerSelection) {
-        return 'Tie'
-    }
+    if (playerSelection == computerSelection) { tieCount++; return 'Tie'; }
     else if (playerSelection == 'rock') {
-        return computerSelection == 'scissors' ? 'You Win! Rock beats Scissors' : 'You Lose! Paper beats Rock'
+        switch (computerSelection) {
+            case 'scissors': winCount++; return 'You Win! Rock beats Scissors';
+            case 'paper': loseCount++; return 'You Lose! Paper beats Rock';
+        }
     }
     else if (playerSelection == 'paper') {
-        return computerSelection == 'rock' ? 'You Win! Paper beats Rock' : 'You Lose! Scissors beats Paper'
+        switch (computerSelection) {
+            case 'rock': winCount++; return 'You Win! Paper beats Rock';
+            case 'scissors': loseCount++; return 'You Lose! Scissors beat Paper';
+        }
     }
     else if (playerSelection == 'scissors') {
-        return computerSelection == 'paper' ? 'You Win! Scissors beats Paper' : 'You Lose! Rock beats Scissors'
+        switch (computerSelection) {
+            case 'paper': winCount++; return 'You Win! Scissors beat Paper';
+            case 'rock': loseCount++; return 'You Lose! Rock beats Scissors';
+        }
     }
-    else return `the value you provided, '${playerSelection}', is not an option`
+    else return `the value you provided, '${playerSelection}', is not an option`;
 }
 
-/*  pure code:
-const playerSelection = prompt(),
-    computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-
-function computerPlay() {
-    let random = Math.trunc(Math.random() * 10),
-        match = (random % 3 === 0) ? 'Rock' : (random % 2 === 0) ? 'Paper' : 'Scissors';
-    return match;
-}
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return 'Tie'
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt();
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = computerPlay(); // calls the function computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+        console.log(playRound(playerSelection, computerSelection));
     }
-    else if (playerSelection == 'Rock') {
-        return computerSelection == 'Paper' ? 'You Lose' : 'You Win'
-    }
-    else if (playerSelection == 'Paper') {
-        return computerSelection == 'Rock' ? 'You Win' : 'You Lose'
-    }
-    else if (playerSelection == 'Scissors') {
-        return computerSelection == 'Rock' ? 'You Lose' : 'You Win'
-    }
-}
-*/
+    return (winCount > loseCount) ? 'You won the game' : 'You lost the game';
+};
